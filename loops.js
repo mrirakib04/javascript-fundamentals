da777fd59b9651d5a1e58617f59cf513b62441a2
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-08-05 23:47:29 +0600

    loops.js added

---
+++ loops.js
@@ -0,0 +1,21 @@
+// for loop
+// Runs a block of code a fixed number of times
+for (let i = 1; i <= 5; i++) {
+  console.log("for loop count:", i);
+}
+
+// while loop
+// Runs while the condition is true
+let j = 1;
+while (j <= 5) {
+  console.log("while loop count:", j);
+  j++;
+}
+
+// do...while loop
+// Always runs at least once, then checks the condition
+let k = 1;
+do {
+  console.log("do...while loop count:", k);
+  k++;
+} while (k <= 5);
